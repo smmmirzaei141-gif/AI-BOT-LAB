48c1cc42f9fb67b52583b010df8c004c3d3cff53
smmmirzaei141-gif <smm.mirzaei141@gmail.com>
2026-09-09 02:00:28 +0330

    connect lab dispatcher commands

--- scripts/lib/dispatcher.sh
+++ scripts/lib/dispatcher.sh
@@ -8,25 +8,26 @@ dispatch_command() {
         install)
             cmd_install "$@"
             ;;
-
         test)
             cmd_test "$@"
             ;;
-
+        build)
+            cmd_build "$@"
+            ;;
         run)
             cmd_run "$@"
             ;;
-
         stop)
             cmd_stop "$@"
             ;;
-
         logs)
             cmd_logs "$@"
             ;;
-
+        clean)
+            cmd_clean "$@"
+            ;;
         *)
-            echo "ERROR: Unknown V2 command: $command"
+            echo "ERROR: Unknown command: $command"
             return 1
             ;;
     esac
